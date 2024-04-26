a45d133cde065e425cbcc44cb532dedb9dfe77c8
byverbel <byverbel@gmail.com>
2024-04-26 02:09:30 -0500

    finished palindrome instance method for string

--- lib/byverbel_palindrome.rb
+++ lib/byverbel_palindrome.rb
@@ -2,7 +2,17 @@
 
 require_relative "byverbel_palindrome/version"
 
-module ByverbelPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# Methods for determining whether a string is a palindrome.
+class String
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    scan(/[a-z]/i).join.downcase
+  end
 end
