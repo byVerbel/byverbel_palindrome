84791bdc7888e90325c2add72fd3cd42dfcbd08b
sllopezv <sllopezv@unal.edu.co>
2024-04-26 01:05:59 -0500

    Initalize repo

---
+++ lib/byverbel_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "byverbel_palindrome/version"
+
+module ByverbelPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/byverbel_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ByverbelPalindrome
+  VERSION = "0.1.0"
+end
